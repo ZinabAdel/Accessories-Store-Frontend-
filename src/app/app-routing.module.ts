d46fd747b5a37bb721013fa426f767d21de654c0
zinabadelhashem@gmail.com <zinabadelhashem@gmail.com>
2021-10-13 19:48:38 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { KnowRingSizeComponent } from './know-ring-size/know-ring-size.component';
 import { LoginComponent } from './User/login/login.component';
 import { RegisterComponent } from './User/register/register.component';
 
 const routes: Routes = [
   {path: 'Register', component:RegisterComponent},
   {path: 'Login', component:LoginComponent},
+  {path: 'HomePage', component:HomePageComponent},
+  {path: 'RingSize', component:KnowRingSizeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavBarComponent } from './Layout/nav-bar/nav-bar.component';
 import { FooterComponent } from './Layout/footer/footer.component';
 import { KnowRingSizeComponent } from './know-ring-size/know-ring-size.component';
-=======
 import { RegisterComponent } from './User/register/register.component';
 import { LoginComponent } from './User/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
->>>>>>> ebcd5e78b4064a98f1cab275b5a454344d6f0274
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     HomePageComponent,
     NavBarComponent,
     FooterComponent,
     KnowRingSizeComponent,
-=======
     RegisterComponent,
     LoginComponent
->>>>>>> ebcd5e78b4064a98f1cab275b5a454344d6f0274
   ],
   imports: [
     BrowserModule,
